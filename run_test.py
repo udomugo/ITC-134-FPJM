2a212cc0b25b0d38d8b2d5c99d810344842fc8da
udomugo <jmorren.contact@gmail.com>
2016-12-06 19:56:29 -0800

    Initial python test script
    Initial upload of test script 'run_test.py' to test final project with Codeship.

---
+++ run_test.py
@@ -0,0 +1,19 @@
+import sys
+
+from finalProject import menu, Conversion, main
+
+def test(did_pass):
+    ''' Print the result of a test. '''
+    linenum = sys._getframe(1).f_lineno
+    if did_pass:
+        msg = 'Test at line {0} ok.'.format(linenum)
+    else:
+        msg = ('Test at line {0} FAILED.'.format(linenum))
+    print(msg)
+
+
+
+test(menu() == print('Temperatur Converter\n1. Celcius to Fahrenheit\n2. Fahrenheit to Celcius\n3. Quit\n'))
+test(Conversion.CtoF(Conversion(0)) == 32)
+test(Conversion.FtoC(Conversion(32)) == 0)
+test(Conversion.CtoF(Conversion(-40)) == -40)
